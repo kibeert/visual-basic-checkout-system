666d56325030568dbbae0ff3c129794b0ace61b2
kibet <107929686+kibeert@users.noreply.github.com>
2024-03-21 13:29:53 +0300

    initial commit

---
+++ Cart.vb
@@ -0,0 +1,14 @@
+﻿Public Class Cart
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Payment.Show()
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Form1.Show()
+    End Sub
+End Class
---
+++ Payment.vb
@@ -0,0 +1,14 @@
+﻿Public Class Payment
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Form1.Show()
+    End Sub
+End Class
